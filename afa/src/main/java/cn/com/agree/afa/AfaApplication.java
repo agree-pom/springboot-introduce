762a42cc3e494bddebf14372ea912852eba61b47
ylife87@sina.com <ylife87@sina.com>
2021-08-03 15:58:25 +0800

    移除忽略database的配置

--- afa/src/main/java/cn/com/agree/afa/AfaApplication.java
+++ afa/src/main/java/cn/com/agree/afa/AfaApplication.java
@@ -11,9 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author mx
  */
 @EnableFeignClients
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class
-})
+@SpringBootApplication()
 public class AfaApplication {
 
     public static void main(String[] args) {
